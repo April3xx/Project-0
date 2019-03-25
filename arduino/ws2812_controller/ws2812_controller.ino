2351b2d91f9fce2132bcd8a480f53a281bb4d53b
sode20yrs <58010207@kmitl.ac.th>
2019-03-25 19:03:24 +0700

    Mac address
    Show arduino's mac address for project day wifi

--- arduino/ws2812_controller/ws2812_controller.ino
+++ arduino/ws2812_controller/ws2812_controller.ino
@@ -17,6 +17,7 @@ const char* ssid     = "iPhone";
 const char* password = "11250112501";
 unsigned int localPort = 7777;
 char packetBuffer[BUFFER_LEN];
+byte mac[6];
 
 // LED strip
 static WS2812 ledstrip;
@@ -40,7 +41,20 @@ void setup() {
         delay(500);
         Serial.print(".");
     }
-    Serial.println("");
+    WiFi.macAddress(mac);
+    Serial.print("MAC : ");
+    Serial.print(mac[5],HEX);
+    Serial.print(":");
+    Serial.print(mac[4],HEX);
+    Serial.print(":");
+    Serial.print(mac[3],HEX);
+    Serial.print(":");
+    Serial.print(mac[2],HEX);
+    Serial.print(":");
+    Serial.print(mac[1],HEX);
+    Serial.print(":");
+    Serial.println(mac[0],HEX);
+    Serial.print("");
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
